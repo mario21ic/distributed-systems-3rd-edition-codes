db48e5a258d86e1c67d83711cb4facd83366d67c
Mario Inga <mario@yarkan.pe>
2018-04-05 10:36:25 -0500

    Ready

---
+++ 04-12/server.py
@@ -0,0 +1,18 @@
+import rpyc
+from constRPYC import * #-
+from rpyc.utils.server import ForkingServer
+
+class DBList(rpyc.Service):
+  value = []
+
+  def exposed_append(self, data):
+    self.value = self.value + [data]
+    return self.value
+
+  def exposed_value(self):
+    return self.value
+
+if __name__ == "__main__":
+  server = ForkingServer(DBList, port = 12345)
+  server.start()
+
